c46830bb8bf0be45116d2a80df684a53e4180142
tom <t.poczos@gmail.com>
2023-03-10 14:33:29 +0100

    processign single row

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,6 +4,8 @@ import cats._
 import cats.data._
 import cats.syntax.all._
 
+import scala.annotation.tailrec
+import scala.collection.:+
 import scala.io.Source
 import scala.util.chaining.scalaUtilChainingOps
 
@@ -13,12 +15,49 @@ object Hello extends Greeting with App {
   val preprocessed = preprocessSource(lines)
   println(preprocessed)
 
-  def preprocessSource(source: Source): List[List[Int]] =
+  def preprocessSource(source: Source): List[List[List[Int]]] =
     source
       .getLines()
       .toList
+      .reverse
       .map(
-        _.split(" ").toList.map(_.toInt))
+        _.split(" ")
+          .toList
+          .map(value => List(value.toInt)))
+
+  def again(graph: List[List[List[Int]]]) = {
+
+    val zeroes = List.fill(0)(graph.head.size + 1).map(zero => List(zero))
+
+    graph.foldLeft((List.empty, zeroes)) {
+      case ((accumulator, previousRow), row) => ???
+    }
+  }
+
+  @tailrec
+  def processRow(accumulator: List[List[Int]], previousRow: List[List[Int]], row: List[List[Int]]): List[List[Int]] = {
+    (previousRow, row) match {
+      case (fstPathPrevRow :: sndPathPrevRow :: restOfprevRow, fstPathRow :: restOfRow) =>
+        if (fstPathPrevRow.sum < sndPathPrevRow.sum)
+          processRow(
+            accumulator :+ (fstPathPrevRow ::: fstPathRow),
+            sndPathPrevRow :: restOfprevRow,
+            restOfRow
+          )
+        else
+          processRow(
+            accumulator :+ (sndPathPrevRow ::: fstPathRow),
+            sndPathPrevRow :: restOfprevRow,
+            restOfRow
+          )
+      case (_, Nil) => accumulator
+    }
+  }
+
+  def messingAround(graph: List[List[Int]]) =
+    graph match {
+      case (fstValfstRow :: sndValFstRow) :: (fstValSndRow :: _) :: _ => List(fstValfstRow)
+    }
 
 }
 
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -16,11 +16,38 @@ class HelloSpec extends AnyWordSpec with Matchers {
            |""".stripMargin
       )
       val expected = List(
-        List(1),
-        List(2, 3),
-        List(4, 5, 6)
+        List(List(4), List(5), List(6)),
+        List(List(2), List(3)),
+        List(List(1)),
       )
       Hello.preprocessSource(source) mustBe expected
+    }
+  }
+
+  "processRow" when {
+    "provided with paths in the previous row" must {
+
+      "return a list of minimum paths for the given row's elements" in {
+        val row = List(
+          List(10),
+          List(20),
+          List(30),
+        )
+        val previousRow = List(
+          List(1, 2),
+          List(7, 8),
+          List(5, 6),
+          List(3, 4),
+        )
+
+        val expected = List(
+          List(1, 2, 10),
+          List(5, 6, 20),
+          List(3, 4, 30),
+        )
+
+        Hello.processRow(List.empty, previousRow, row) mustBe expected
+      }
     }
   }
 
